cdc82951aebd01ffaa7205f982988328872ba997
FrogTravel <e.levchenko@innopolis.ru>
2017-04-11 01:40:25 +0300

    Front end editing

--- eiffel_project/src/admin_src/adminscripts/clicker.js
+++ eiffel_project/src/admin_src/adminscripts/clicker.js
@@ -1,13 +1,39 @@
-$("#askbtn").click( function(event){
-	console.log("btn clicked");
-	if($("select").find(":selected").text() === "All publications in a given year"){
+// $("#askbtn").click( function(event){
+// 	console.log("btn clicked");
+// 	if($("select").find(":selected").text() === "All publications in a given year"){
+// 		var newElement = "<input id=\"givenYear\" type=\"number\" name=\"year\"></input>"+
+// 		"<button id=\"submbtn\">Submit</button>";
+// 		$("#additionalInfo").html(newElement);
+// 		setUpSbmButton();
+// 	}
+
+// 	if($("select").find(":selected").text() === "Summary information about given unit in given number of years"){
+// 		var newElement = "<input id=\"givenUnit\" type=\"text\" name=\"unit\"></input>" + 
+// 		"<input id=\"givenNumberOfYears\" type=\"number\" name=\"years\"></input>" +
+// 		"<button id=\"submbtn\">Submit</button>";
+// 		$("#additionalInfo").html(newElement);
+// 		setUpSbmButton();
+// 	}
+
+// 	if($("select").find(":selected").text() === "Courses between two dates local"){
+// 		var newElement = "<input id=\"dateOne\" type=\"text\" name=\"dateOne\"></input>" + 
+// 		"<input id=\"dateTwo\" type=\"text\" name=\"dateTwo\"></input>" +
+// 		"<button id=\"submbtn\">Submit</button>";
+// 		$("#additionalInfo").html(newElement);
+// 		setUpSbmButton();
+// 	}
+// })
+
+$(".buttonChoice").click( function(event){
+	console.log($(this).text());
+	if($(this).text() === "All publications in a given year"){
 		var newElement = "<input id=\"givenYear\" type=\"number\" name=\"year\"></input>"+
 		"<button id=\"submbtn\">Submit</button>";
 		$("#additionalInfo").html(newElement);
 		setUpSbmButton();
 	}
 
-	if($("select").find(":selected").text() === "Summary information about given unit in given number of years"){
+	if($(this).text() === "Summary information about given unit in given number of years"){
 		var newElement = "<input id=\"givenUnit\" type=\"text\" name=\"unit\"></input>" + 
 		"<input id=\"givenNumberOfYears\" type=\"number\" name=\"years\"></input>" +
 		"<button id=\"submbtn\">Submit</button>";
@@ -15,13 +41,14 @@ $("#askbtn").click( function(event){
 		setUpSbmButton();
 	}
 
-	if($("select").find(":selected").text() === "Courses between two dates local"){
+	if($(this).text() === "Courses between two dates local"){
 		var newElement = "<input id=\"dateOne\" type=\"text\" name=\"dateOne\"></input>" + 
 		"<input id=\"dateTwo\" type=\"text\" name=\"dateTwo\"></input>" +
 		"<button id=\"submbtn\">Submit</button>";
 		$("#additionalInfo").html(newElement);
 		setUpSbmButton();
 	}
+
 })
 
 function setUpSbmButton(){
--- eiffel_project/src/scripts/newRowsAdder.js
+++ eiffel_project/src/scripts/newRowsAdder.js
@@ -3,17 +3,17 @@ $(document).ready(function(){
 	$("#addbtncourses").click( function(even){
 		var newElement = "<div id=\"" + coursesIndex  + "course" + "\" class=\"bigform courses\">" +
 			"<p class=\"inline number\">" + (coursesIndex+2) + ". </p>" +
-			"<input id=\"COURSENAME\" type=\"text\" name=\"COURSENAME\">" +
-			"<select id=\"semesterSelectorCourses\">" +
+			"<input id=\"COURSENAME\" class=\"mandatory newElement\" type=\"text\" placeholder=\"Course name\" name=\"COURSENAME\">" +
+			"<select id=\"semesterSelectorCourses\" class=\"newElement\">" +
 				 "<option>Fall</option>" +
 				 "<option>Spring</option>" +
 			 "</select>" +
-			 "<select id=\"levelSelectorCourses\">" +
+			 "<select id=\"levelSelectorCourses\" class=\"newElement\"> " +
 				"<option>Bachelor</option>" +
 				"<option>Master</option>" +
 				"<option>PhD</option>" +
 			"</select>" +
-			"<input type=\"number\" name=\"APPROXIMATESTUDENTNUMBER\" id=\"APPROXIMATESTUDENTNUMBER\"></div>";
+			"<input placeholder=\"Amount of students\" class=\"mandatory newElement\"  type=\"number\" name=\"APPROXIMATESTUDENTNUMBER\" id=\"APPROXIMATESTUDENTNUMBER\"></div>";
 
 		coursesIndex += 1;
 		
@@ -31,16 +31,16 @@ $(document).ready(function(){
 	$("#addbtnexams").click( function(even){
 		var newElement = "<div class=\"bigform exams\" id=\"" + examsIndex + "exam\">" +
 			"<p class=\"inline number\">" + (examsIndex+2) + ". </p>" +	
-			"<input id=\"courseName\" type=\"text\" name=\"COURSENAME\">" +
-			"<select id=\"examsSelector\">" +
+			"<input id=\"courseName\" class=\"mandatory newElement\" placeholder=\"Course name\"  type=\"text\" name=\"COURSENAME\">" +
+			"<select id=\"examsSelector\" class=\"newElement\">" +
 				"<option>Fall</option>" +
 				"<option>Spring</option>" +
 			"</select>" +
-			"<select id=\"kindOfExams\">" +
+			"<select id=\"kindOfExams\" class=\"newElement\">" +
 				"<option>Final</option>" +
 				"<option>Repetition</option>" +
 			"</select>" +
-			"<input id=\"approxStudNumb\" type=\"number\" name=\"APPROXIMATESTUDENTNUMBER\">" +
+			"<input id=\"approxStudNumb\" class=\"mandatory newElement\" placeholder=\"Amount of students\" type=\"number\" name=\"APPROXIMATESTUDENTNUMBER\">" +
 		"</div>";
 
 		examsIndex++;
@@ -60,8 +60,8 @@ $(document).ready(function(){
 	var supervisedIndex = 0;
 	$("#addbtnstudsupervised").click( function(even){
 		var newElement = "<div id=\"" + supervisedIndex + "supervised\" class=\"bigform studsupervised\">" + 
-			"<input id=\"studentNameStudSupervised\" type=\"text\" name=\"name\">" +
-			"<select id=\"levelStudSupervised\">" +
+			"<input id=\"studentNameStudSupervised\" class=\"mandatory newElement\" placeholder=\"Student name\" type=\"text\" name=\"name\">" +
+			"<select id=\"levelStudSupervised\" class=\"newElement\">" +
 				"<option>Bachelor Thesis</option>" +
 				"<option>Master Thesis</option>" +
 				"<option>Subber Internship</option>" +
@@ -81,10 +81,10 @@ $(document).ready(function(){
 	//Completed student reports
 	var studReportsIndex = 0;
 	$("#addbtnstudreports").click( function(even){
-		var newElement = "<div id=\"" + studReportsIndex + "studrep\"class=\"bigform reports\">" +
-			"<input id=\"studentNameReports\" type=\"text\" name=\"name\">" +
-			"<input id=\"titleOfReportStudent\" type=\"text\" name=\"titleofreport\">" +
-			"<input id=\"plansForPublication\" class=\"notMandatory\" type=\"text\" name=\"plans\">" +
+		var newElement = "<div id=\"" + studReportsIndex + "studrep\" class=\"bigform reports\">" +
+			"<input id=\"studentNameReports\" class=\"mandatory newElement\" placeholder=\"Student name\" type=\"text\" name=\"name\">" +
+			"<input id=\"titleOfReportStudent\" class=\"mandatory newElement\" placeholder=\"Title of Report\" type=\"text\" name=\"titleofreport\">" +
+			"<input id=\"plansForPublication\" class=\"newElement\" placeholder=\"Publication plans\" class=\"notMandatory\" type=\"text\" name=\"plans\">" +
 		"</div>";
 
 		studReportsIndex++;
@@ -102,15 +102,15 @@ $(document).ready(function(){
 	var compPhDIndex = 0;
 	$("#addbtncompphd").click( function(even){
 		var newElement = "<div id=\"" + compPhDIndex + "compPhD\" class=\"bigform theses\">" +
-			"<input id=\"studentNameTheses\" type=\"text\" name=\"name\">" +
-			"<select id=\"degreeSelector\">" + 
+			"<input id=\"studentNameTheses\" class=\"newElement\" placeholder=\"Student name\" type=\"text\" name=\"name\">" +
+			"<select id=\"degreeSelector\" class=\"newElement\">" + 
 				"<option>PhD</option>" +
 				"<option>Dr. Sc.</option>" +
 			"</select>" +
-			"<input id=\"supervisorName\" type=\"text\" name=\"name\">" +
-			"<input id=\"otherMembersName\" type=\"type\" name=\"nameOfComitee\">" +
-			"<input id=\"nameOfDegreeGrantingInstitution\" type=\"text\" name=\"nameOfInstitution\">" +
-			"<input id=\"titleOfTheses\" type=\"text\" name=\"titleOfTheses\">" +
+			"<input id=\"supervisorName\" class=\"newElement\" placeholder=\"Name of supervisor\" type=\"text\" name=\"name\">" +
+			"<input id=\"otherMembersName\" class=\"newElement\" placeholder=\"Other members name\" type=\"type\" name=\"nameOfComitee\">" +
+			"<input id=\"nameOfDegreeGrantingInstitution\" class=\"newElement\" placeholder=\"Degree-Granting institution\" type=\"text\" name=\"nameOfInstitution\">" +
+			"<input id=\"titleOfTheses\" class=\"newElement\" placeholder=\"Title of dissertation\" type=\"text\" name=\"titleOfTheses\">" +
 		"</div>"
 
 		compPhDIndex++;
@@ -130,10 +130,10 @@ $(document).ready(function(){
 		console.log("123");
 		var newElement = "<div id=\"" + grantsIndex + "grants\" class=\"bigform grants\">" + 
 			"<p class=\"inline number\">" + (grantsIndex+2) + ". </p>" +
-			"<input id=\"titleOfProject\" type=\"text\" name=\"name\">" +
-			"<input id=\"grantingAgency\" type=\"text\" name=\"agency\">" +
-			"<input  id=\"durationGrants\" type=\"number\" name=\"date\">" +
-			"<input id=\"amountOfGrant\" type=\"text\" name=\"amountOfGrant\">" +
+			"<input id=\"titleOfProject\" class=\"mandatory newElement\" placeholder=\"Title of project\" type=\"text\" name=\"name\">" +
+			"<input id=\"grantingAgency\" class=\"mandatory newElement\" placeholder=\"Granting agency\" type=\"text\" name=\"agency\">" +
+			"<input  id=\"durationGrants\" class=\"mandatory newElement\" placeholder=\"Period covered by grant (in years)\" type=\"number\" name=\"date\">" +
+			"<input id=\"amountOfGrant\" class=\"mandatory newElement\" placeholder=\"Amount of grant\" type=\"text\" name=\"amountOfGrant\">" +
 		"</div>";
 		grantsIndex++;
 
@@ -154,12 +154,14 @@ $(document).ready(function(){
 	$("#addbtnResearchProj").click( function(even){
 		var newElement = "<div id=\"" + researchProjIndex + "rP\" class=\"bigform researchProj\">" +
 			"<p class=\"inline number\">"+ (researchProjIndex+2) + ". </p>" +
-			"<input id=\"titleOfProjectResearch\" type=\"text\" name=\"name\">" +
-			"<input id=\"IUpersonnel\" type=\"text\" name=\"IUpersonnel\">" +
-			"<input id=\"externalPersonnel\" type=\"text\" name=\"externalPersonnel\">" +
-			"<input  class=\"datepicker\" id=\"startDateOfProject\" type=\"text\" name=\"date\">" + 
-			"<input  class=\"datepicker\" id=\"endDateOfProject\" type=\"text\" name=\"date\">" +
-			"<input id=\"sourceOfFinance\" type=\"text\" name=\"sourceOfFinance\">" +
+			"<input id=\"titleOfProjectResearch\" class=\"mandatory newElement\" placeholder=\"Title of project\" type=\"text\" name=\"name\">" +
+			"<input id=\"IUpersonnel\" class=\"mandatory newElement\" placeholder=\"IU persons involved\" type=\"text\" name=\"IUpersonnel\">" +
+			"<input id=\"externalPersonnel\" class=\"mandatory newElement\" placeholder=\"External persons involved\" type=\"text\" name=\"externalPersonnel\">" +
+			"<div></div>" + 
+			"<p class=\"inline\" style=\"color: transparent;\">11</p>" +
+			"<input  class=\"datepicker newElement\" id=\"startDateOfProject\" placeholder=\"Start date\" type=\"text\" name=\"date\">" + 
+			"<input  class=\"datepicker newElement\" id=\"endDateOfProject\" placeholder=\"End date\" type=\"text\" name=\"date\">" +
+			"<input id=\"sourceOfFinance\" class=\"mandatory newElement\" placeholder=\"Source of financing\" type=\"text\" name=\"sourceOfFinance\">" +
 		"</div>";
 		researchProjIndex++;
 
@@ -178,12 +180,14 @@ $(document).ready(function(){
 	$("#addbtnResearchColl").click( function(even){
 		var newElement = "<div id=\"" + resColl + "rC\" class=\"bigform researchColl\">" +
 			"<p class=\"inline number\">" + (resColl+2) +". </p>" +
-			"<input id=\"countryOfInst\" type=\"text\" name=\"country\">" +
-			"<input id=\"nameOfInstitution\" type=\"text\" name=\"nameOfInstitution\">" +
-			"<input id=\"departmentOfUnit\" class=\"notMandatory\" type=\"text\" name=\"departmentOfUnit\">" +
-			"<input id=\"nameOfPrincipal\" type=\"text\" name=\"nameOfPrincipal\">" +
-			"<input id=\"otherImportantContacts\" type=\"text\" name=\"otherImportantContacts\">" +
-			"<input id=\"natureOfColl\" type=\"text\" name=\"natureOfColl\">" +
+			"<input id=\"countryOfInst\" class=\"mandatory newElement\" placeholder=\"Country of institution\" type=\"text\" name=\"country\">" +
+			"<input id=\"nameOfInstitution\" class=\"mandatory newElement\" placeholder=\"Name of institution\" type=\"text\" name=\"nameOfInstitution\">" +
+			"<input id=\"departmentOfUnit\" class=\"notMandatory newElement\" placeholder=\"Department\" type=\"text\" name=\"departmentOfUnit\">" +
+			"<div></div>" + 
+			"<p class=\"inline\" style=\"color: transparent;\">11</p>" +
+			"<input id=\"nameOfPrincipal\" class=\"mandatory newElement\" placeholder=\"Name of Principal\" type=\"text\" name=\"nameOfPrincipal\">" +
+			"<input id=\"otherImportantContacts\" class=\"mandatory newElement\" placeholder=\"Other important contacts\" type=\"text\" name=\"otherImportantContacts\">" +
+			"<input id=\"natureOfColl\" class=\"mandatory newElement\" placeholder=\"Nature of collaboration\" type=\"text\" name=\"natureOfColl\">" +
 		"</div>";
 
 		resColl++;
@@ -202,7 +206,7 @@ $(document).ready(function(){
 	$("#addbtnconference").click( function(even){
 		var newElement = "<div id=\"" + confPubl + "cP\"class=\"bigform conferencePubl\">" + 
 			"<p class=\"inline number\">" + (confPubl+2) + ". </p>" +
-			"<input id=\"publications\" type=\"text\" name=\"publications\">" +
+			"<input id=\"publications\" class=\"mandatory\" placeholder=\"Name of publication\" type=\"text\" name=\"publications\">" +
 		"</div>";
 		confPubl++;
 
@@ -222,7 +226,7 @@ $(document).ready(function(){
 	$("#addbtnjournalpubl").click( function(even){
 		var newElement = "<div id=\"" + jourPubl + "jP\" class=\"bigform journalPubl\">" +
 			"<p class=\"inline number\">" + (jourPubl+2) + ". </p>" +
-			"<input id=\"publications\" type=\"text\" name=\"publications\">" +
+			"<input id=\"publications\" class=\"mandatory\" placeholder=\"Name of publication\" type=\"text\" name=\"publications\">" +
 		"</div>";
 		jourPubl++;
 
